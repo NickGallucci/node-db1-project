001d6636444ef567cd17deb9292cb02e1cf41839
CryptoN999 <nicholasgallucci4@gmail.com>
2020-01-31 17:43:14 -0800

    basic router, working, adding more

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const db = require('./data/dbConfig.js');
 
+const accountsRouter = require('./accounts/accountsRouter')
+
 const server = express();
 
 server.use(express.json());
 
-server.use('/api/accounts', accountRouter) //connects the router to the server
+server.use('/api/accounts', accountsRouter) //connects the router to the server
 
 server.get('/', (req, res) => {
     return res.send('<h1> Que dialup tone! <h2>')
